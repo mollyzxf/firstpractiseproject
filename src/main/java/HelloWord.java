b22af582d15c5fa0f4963f2480222eedcd4230b2
molly <465334039@qq.com>
2018-09-24 15:40:23 +0800

    first commit

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
+/**
+ * @author
+ * @date 2018/09/22
+ */
 public class HelloWord {
+
+    public static void main(String[]args){
+        System.out.println("helloGithub,I'm here!");
+    }
+
 }
--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
+/**
+ * @author
+ * @date 2018/09/23
+ */
 public class Test1 {
+
+    public static void main(String[] args) {
+
+
+    String str1 = "china";
+    String str2 = new String("china");
+    String s1 = "chin";
+    String s2 = "a";
+    String s3 = s1 + s2;
+    System.out.println(str1== str2);
+    System.out.println(str1==str2.intern());
+
+    System.out.println(s1==s3);
+    System.out.println(s1==s3.intern());
+}
 }
