9d0b03b22ca2114220a484b6adb75dab50718ad6
molly <465334039@qq.com>
2018-09-24 16:08:46 +0800

    test2

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -9,7 +9,7 @@ public class Test1 {
 
     String str1 = "china";
     String str2 = new String("china");
-    String s1 = "chin";
+    String s1 = "chinn";
     String s2 = "a1";
     String s3 = s1 + s2;
     System.out.println(str1== str2);
